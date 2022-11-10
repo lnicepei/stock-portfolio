49e254388a781b4ad004b0d74f89d5fd80ca71e0
lnicepei <dmitrii.shaidorov@gmail.com>
2022-11-10 14:05:40 +0300

    Create initial markup

---
+++ src/Header/Header.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import styled from "styled-components";
+import HeaderCoin from "./HeaderCoin/HeaderCoin";
+import UserCorner from "./UserCorner/UserCorner";
+
+const StyledHeader = styled.header`
+  display: flex;
+`;
+
+const StyledCardHolder = styled.div`
+  display: flex;
+`;
+
+const Header = ({ listOfCoins, userPortfolio, setUserPortfolio }) => {
+  const topThreeCoinCards = listOfCoins
+    .slice(0, 3)
+    .map((coin) => (
+      <HeaderCoin
+        rank={coin.rank}
+        name={coin.name}
+        symbol={coin.symbol}
+        price={coin.priceUsd.substring(0, 7)}
+      />
+    ));
+
+  return (
+    <StyledHeader>
+      <StyledCardHolder>{topThreeCoinCards}</StyledCardHolder>
+      <UserCorner
+        listOfCoins={listOfCoins}
+        userPortfolio={userPortfolio}
+        setUserPortfolio={setUserPortfolio}
+      />
+    </StyledHeader>
+  );
+};
+
+export default Header;
