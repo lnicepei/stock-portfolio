11ec50f086d459b05ed9b0f4fdea8e5f9330e9de
lnicepei <dmitrii.shaidorov@gmail.com>
2022-11-11 15:36:24 +0300

    Move RenderLineChart, Additional to separate folders

--- src/Main/CoinList/CoinPage/CoinPage.tsx
+++ src/Main/CoinList/CoinPage/CoinPage.tsx
@@ -1,17 +1,9 @@
-import React, { useEffect, useState } from "react";
-import styled from "styled-components";
-import { useParams } from "react-router-dom";
-import {
-  ResponsiveContainer,
-  AreaChart,
-  XAxis,
-  YAxis,
-  Area,
-  Tooltip,
-  CartesianGrid,
-} from "recharts";
 import Axios from "axios";
-import { format, parseISO } from "date-fns";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import styled from "styled-components";
+import AdditionalCoinInfo from "./AdditionalCoinInfo/AdditionalCoinInfo";
+import RenderLineChart from "./RenderLineChart/RenderLineChart";
 
 const StyledCoinPage = styled.div`
   display: flex;
@@ -38,50 +30,11 @@ const CoinPage = ({ coinInfo, listOfCoins }) => {
     });
   }, []);
 
-  console.log(additionalCoinInfo);
-
-  const renderLineChart = (
-    <ResponsiveContainer width="75%" height={400}>
-      <AreaChart data={coinHistory.data}>
-        <defs>
-          <linearGradient id="color" x1="0" y1="0" x2="0" y2="1">
-            <stop offset="0%" stopColor="#2451B7" stopOpacity={0.4} />
-            <stop offset="75%" stopColor="#2451B7" stopOpacity={0.05} />
-          </linearGradient>
-        </defs>
-
-        <Area dataKey="priceUsd" stroke="#2451B7" fill="url(#color)" />
-
-        <XAxis
-          dataKey="date"
-          tickFormatter={(str) => {
-            const date = new Date(str || "");
-            return date.toDateString();
-          }}
-        />
-
-        <YAxis
-          dataKey="priceUsd"
-          tickCount={8}
-          tickFormatter={(number) => `$${+number.toFixed(2)}`}
-        />
-
-        {/* <Tooltip content={<CustomTooltip />} /> */}
-        <Tooltip />
-
-        <CartesianGrid opacity={0.1} vertical={false} />
-      </AreaChart>
-    </ResponsiveContainer>
-  );
-
   return (
     <StyledCoinPage>
-      {fullInfo?.id} {renderLineChart}{" "}
-      {Object.keys(additionalCoinInfo).map((key) => (
-        <div>
-          {key}: {additionalCoinInfo[key]}
-        </div>
-      ))}
+      {fullInfo?.id}
+      <RenderLineChart coinHistory={coinHistory} />
+      <AdditionalCoinInfo additionalCoinInfo={additionalCoinInfo} />
     </StyledCoinPage>
   );
 };
