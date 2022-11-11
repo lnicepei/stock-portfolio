64c4dda608a4797a63da2b6429265edf7fbbfbc8
lnicepei <dmitrii.shaidorov@gmail.com>
2022-11-11 16:20:57 +0300

    Create default styled button

--- src/App/App.styles.ts
+++ src/App/App.styles.ts
@@ -1,12 +1,41 @@
 import styled from "styled-components";
 
 export const Card = styled.div`
-  margin: 5px;
-  flex: 1 1;
   display: flex;
   align-items: center;
-  box-shadow: rgba(149, 157, 165, 0.5) 0px 8px 24px;
+  flex: 1 1;
+  margin: 5px;
+  padding: 5px;
+  border: 1px solid #e7ecef;
+  box-shadow: 0 2px 4px rgba(33, 36, 41, 0.05);
   box-sizing: border-box;
   border-radius: 10px;
-  padding: 5px;
+  cursor: pointer;
+  transition: all 0.1s ease-in-out;
+
+  &:hover {
+    box-shadow: 0 10px 16px rgba(0, 0, 0, 0.05);
+  }
+`;
+
+export const Button = styled.button`
+  background-color: rgba(51, 51, 51, 0.05);
+  border-radius: 8px;
+  border-width: 0;
+  color: #333333;
+  cursor: pointer;
+  display: inline-block;
+  font-family: "Haas Grot Text R Web", "Helvetica Neue", Helvetica, Arial,
+    sans-serif;
+  font-size: 14px;
+  font-weight: 500;
+  line-height: 20px;
+  list-style: none;
+  margin: 0;
+  padding: 10px 12px;
+  text-align: center;
+  transition: all 200ms;
+  vertical-align: baseline;
+  white-space: nowrap;
+  user-select: none;
 `;
