e1c363bd973349bb5119b137e8e1c7495cbea867
lnicepei <dmitrii.shaidorov@gmail.com>
2022-11-11 15:34:07 +0300

    Create styled Card component

---
+++ src/App/App.styles.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Card = styled.div`
+  margin: 5px;
+  flex: 1 1;
+  display: flex;
+  align-items: center;
+  box-shadow: rgba(149, 157, 165, 0.5) 0px 8px 24px;
+  box-sizing: border-box;
+  border-radius: 10px;
+  padding: 5px;
+`;
