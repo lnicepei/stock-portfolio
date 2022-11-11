6381c5411f85ac0ec79442098f4e7075d39ac775
lnicepei <dmitrii.shaidorov@gmail.com>
2022-11-11 16:24:28 +0300

    Make use of styled Card component

--- src/Main/CoinList/Coin/Coin.tsx
+++ src/Main/CoinList/Coin/Coin.tsx
@@ -1,11 +1,20 @@
-import { Card } from "../../../App/App.styles";
+import styled from "styled-components";
+import { Button, Card } from "../../../App/App.styles";
+
+const StyledCoin = styled(Card)`
+  padding: 20px;
+  margin: 5px;
+  justify-content: space-between;
+`;
 
 const Coin = ({ coin, handleBuyMenuOpen, handleCoinMenuOpen }) => {
   return (
-    <Card onClick={handleCoinMenuOpen}>
-      <button onClick={handleBuyMenuOpen}>Buy</button>
-      {coin.rank}, {coin.name}, {coin.price}, {coin.symbol}
-    </Card>
+    <StyledCoin onClick={handleCoinMenuOpen}>
+      <div>
+        {coin.rank}, {coin.id}, {coin.priceUsd}
+      </div>
+      <Button onClick={handleBuyMenuOpen}>Buy</Button>
+    </StyledCoin>
   );
 };
 
