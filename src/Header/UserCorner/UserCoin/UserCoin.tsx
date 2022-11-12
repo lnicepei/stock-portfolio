0f495d7e0b18a1f9e6c27c54483ef3e17fd4a214
lnicepei <dmitrii.shaidorov@gmail.com>
2022-11-12 10:04:07 +0300

    Navigate to coin page on card click

--- src/Header/UserCorner/UserCoin/UserCoin.tsx
+++ src/Header/UserCorner/UserCoin/UserCoin.tsx
@@ -1,22 +1,30 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import { Button, Card } from "../../../App/App.styles";
 
 const UserCoin = ({ coin, setUserPortfolio }) => {
-  const deleteCoin = () => {
+  const navigate = useNavigate();
+  const deleteCoin = (e) => {
+    e.stopPropagation();
     //TODO: Delete from localstorage too
     setUserPortfolio((prevUserPortfolio) =>
       prevUserPortfolio.filter((coinInPortfolio) => {
-        if (coinInPortfolio.name !== coin.name) {
+        if (coinInPortfolio.id !== coin.id) {
           return coinInPortfolio;
         }
       })
     );
   };
 
+  const openCoinPage = () => {
+    navigate(coin.id);
+  };
+
   return (
-    <div>
-      {coin.name}
-      <button onClick={deleteCoin}>Delete </button>
-    </div>
+    <Card onClick={openCoinPage}>
+      {coin?.id}
+      <Button onClick={(e) => deleteCoin(e)}>Delete </Button>
+    </Card>
   );
 };
 
