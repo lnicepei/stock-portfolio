faeadd3d794377a91cf0cbb677b3d15d6a599ad2
lnicepei <dmitrii.shaidorov@gmail.com>
2022-11-12 11:20:35 +0300

    Declare global types for coins

--- src/Main/Main.tsx
+++ src/Main/Main.tsx
@@ -1,9 +1,8 @@
-import React, { useState, useEffect, useRef } from "react";
-import { Routes, Route, BrowserRouter, useNavigate } from "react-router-dom";
+import React, { useRef, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import BuyMenu from "./CoinList/BuyMenu/BuyMenu";
 import Coin from "./CoinList/Coin/Coin";
-import CoinPage from "./CoinList/CoinPage/CoinPage";
 
 const StyledMain = styled.main`
   display: flex;
@@ -12,21 +11,53 @@ const StyledMain = styled.main`
   padding: 0;
 `;
 
-const Main = ({
+declare global {
+  interface APICoin {
+    changePercent24Hr: string;
+    explorer: string;
+    id: string;
+    marketCapUsd: string;
+    maxSupply: string;
+    name: string;
+    priceUsd: string;
+    rank: string;
+    supply: string;
+    symbol: string;
+    volumeUsd24Hr: string;
+    vwap24Hr: string;
+  }
+
+  interface UserCoin {
+    id: string;
+    quantity: number;
+    buyPrice: number;
+  }
+}
+
+type Props = {
+  currentCoin: UserCoin;
+  setCurrentCoin: React.Dispatch<React.SetStateAction<UserCoin>>;
+  listOfCoins: APICoin[];
+  setUserPortfolio: React.Dispatch<React.SetStateAction<UserCoin[]>>;
+};
+
+const Main: React.FC<Props> = ({
   currentCoin,
   setCurrentCoin,
   listOfCoins,
   setUserPortfolio,
 }) => {
-  const buyMenuRef = useRef(null);
+  const buyMenuRef = useRef<HTMLDivElement | null>(null);
   const [isBuyMenuOpen, setIsBuyMenuOpen] = useState(false);
   const navigate = useNavigate();
 
   const buy = () => {
     setIsBuyMenuOpen(false);
-    setUserPortfolio((prevUserPortfolio) => {
-      if (prevUserPortfolio.some((coin) => coin.id === currentCoin.id)) {
-        return prevUserPortfolio.map((coin) => {
+    setUserPortfolio((prevUserPortfolio: UserCoin[]) => {
+      if (
+        prevUserPortfolio.some((coin: UserCoin) => coin.id === currentCoin.id)
+      ) {
+        return prevUserPortfolio.map((coin: UserCoin) => {
           if (coin.id === currentCoin.id) {
             return {
               ...coin,
@@ -46,7 +77,7 @@ const Main = ({
     });
   };
 
-  const handleBuyMenuOpen = (e, coin) => {
+  const handleBuyMenuOpen = (e: React.SyntheticEvent, coin: APICoin) => {
     e.stopPropagation();
     setCurrentCoin((prevCurrentCoin) => {
       return {
@@ -57,8 +88,14 @@ const Main = ({
     setIsBuyMenuOpen(true);
   };
 
-  const handleCoinMenuOpen = (e, coin) => {
-    if (buyMenuRef.current && !buyMenuRef.current.contains(e.target)) {
+  const handleCoinMenuOpen = (
+    e: React.BaseSyntheticEvent<Event, EventTarget & Element, EventTarget>,
+    coin: APICoin
+  ) => {
+    if (
+      buyMenuRef.current &&
+      !buyMenuRef.current.contains(e.target as HTMLDivElement)
+    ) {
       setIsBuyMenuOpen(false);
     } else {
       navigate(coin.id);
@@ -81,8 +118,12 @@ const Main = ({
           <Coin
             key={coin.id}
             coin={coin}
-            handleBuyMenuOpen={(e) => handleBuyMenuOpen(e, coin)}
-            handleCoinMenuOpen={(e) => handleCoinMenuOpen(e, coin)}
+            handleBuyMenuOpen={(e: React.SyntheticEvent) =>
+              handleBuyMenuOpen(e, coin)
+            }
+            handleCoinMenuOpen={(e: React.SyntheticEvent) =>
+              handleCoinMenuOpen(e, coin)
+            }
           />
         );
       })}
