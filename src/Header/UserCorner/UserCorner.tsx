937d8519140da5ea9fb30e1f5509d88b42596cb9
lnicepei <dmitrii.shaidorov@gmail.com>
2022-11-12 16:08:29 +0300

    Create modal menu, toggled buy a button

--- src/Header/UserCorner/UserCorner.tsx
+++ src/Header/UserCorner/UserCorner.tsx
@@ -1,42 +1,75 @@
-import React from "react";
+import React, { useState } from "react";
+import styled from "styled-components";
+import { Card } from "../../App/App.styles";
 import { HeaderProps } from "../Header";
 import UserCoin from "./UserCoin/UserCoin";
+
+const StyledUserCorner = styled.div`
+  display: flex;
+`;
+
+const StyledUserCornerModal = styled.div`
+  z-index: 2;
+`;
+
+const StyledModalToggle = styled(Card)`
+  align-items: flex-start;
+  z-index: 2;
+  height: auto;
+`;
 
 const UserCorner: React.FC<HeaderProps> = ({
   listOfCoins,
   userPortfolio,
   setUserPortfolio,
 }) => {
-  const userCoins = userPortfolio.map((coin) => (
-    <UserCoin
-      key={coin.id}
-      coin={listOfCoins.find((listCoin) => listCoin.id === coin.id)}
-      setUserPortfolio={setUserPortfolio}
-    />
-  ));
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   const previousUserMoney = userPortfolio.reduce(
     (total: number, coin: UserCoin) => total + +coin.quantity * coin.buyPrice,
     0
   );
 
-  const currentUserMoney = userPortfolio.reduce(
-    (total: number, coin: UserCoin) =>
-      total +
-      +coin.quantity *
-        listOfCoins.find((coinInList: APICoin) => coinInList.id === coin.id)
-          ?.priceUsd,
-    0
-  );
+  const toggleModal = () => {
+    setIsModalOpen((prevIsModalOpen) => !prevIsModalOpen);
+  };
+
+  const currentUserMoney =
+    userPortfolio.reduce(
+      (total: number, coin: UserCoin) =>
+        total +
+        +coin.quantity *
+          +listOfCoins.filter?.(
+            (coinInList: APICoin) => coinInList.id === coin.id
+          )[0]?.priceUsd,
+      0
+    ) || 0;
+
+  const userCoins = userPortfolio.map((coin) => (
+    <UserCoin
+      key={coin.id}
+      toggleModal={toggleModal}
+      coin={listOfCoins.find((listCoin) => listCoin.id === coin.id)}
+      setUserPortfolio={setUserPortfolio}
+    />
+  ));
 
   return (
-    <div>
+    <StyledUserCorner>
       <div>
         {currentUserMoney} USD {currentUserMoney - previousUserMoney}{" "}
         {100 - (currentUserMoney / previousUserMoney || 1) * 100}
       </div>
-      {userCoins} {previousUserMoney} {currentUserMoney}
-    </div>
+      <StyledModalToggle onClick={toggleModal}>
+        <div>Modal</div>
+        {isModalOpen && (
+          <StyledUserCornerModal>{userCoins}</StyledUserCornerModal>
+        )}
+      </StyledModalToggle>
+
+      <div>{previousUserMoney}</div>
+      <div>{currentUserMoney}</div>
+    </StyledUserCorner>
   );
 };
 
