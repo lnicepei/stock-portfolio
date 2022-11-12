0e03eeca6e046720671a1df1fb85471c0eadae23
lnicepei <dmitrii.shaidorov@gmail.com>
2022-11-12 11:22:21 +0300

    Add types to props, handle loading page behavior

--- src/Header/UserCorner/UserCoin/UserCoin.tsx
+++ src/Header/UserCorner/UserCoin/UserCoin.tsx
@@ -2,28 +2,34 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import { Button, Card } from "../../../App/App.styles";
 
-const UserCoin = ({ coin, setUserPortfolio }) => {
+type Props = {
+  coin: APICoin | undefined;
+  setUserPortfolio: React.Dispatch<React.SetStateAction<UserCoin[]>>;
+};
+
+const UserCoin: React.FC<Props> = ({ coin, setUserPortfolio }) => {
   const navigate = useNavigate();
-  const deleteCoin = (e) => {
+  const deleteCoin = (e: React.SyntheticEvent) => {
     e.stopPropagation();
     //TODO: Delete from localstorage too
     setUserPortfolio((prevUserPortfolio) =>
-      prevUserPortfolio.filter((coinInPortfolio) => {
-        if (coinInPortfolio.id !== coin.id) {
+      prevUserPortfolio.filter((coinInPortfolio: UserCoin) => {
+        if (coinInPortfolio.id !== coin!.id) {
           return coinInPortfolio;
         }
+        return;
       })
     );
   };
 
   const openCoinPage = () => {
-    navigate(coin.id);
+    navigate(coin!.id);
   };
 
   return (
     <Card onClick={openCoinPage}>
       {coin?.id}
-      <Button onClick={(e) => deleteCoin(e)}>Delete </Button>
+      <Button onClick={deleteCoin}>Delete</Button>
     </Card>
   );
 };
