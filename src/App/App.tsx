6bc1b1a72ccdcafb901a2314649be32a5b43bc84
lnicepei <dmitrii.shaidorov@gmail.com>
2022-11-13 14:00:16 +0300

    Create buyContext, wrap Main and CoinPage in it

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,10 +1,20 @@
 import Axios from "axios";
-import React, { useEffect, useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import "./App.css";
 import Header from "../Header/Header";
 import CoinPage from "../Main/CoinList/CoinPage/CoinPage";
 import Main from "../Main/Main";
+import "./App.css";
+
+interface BuyContext {
+  currentCoin: UserCoin;
+  setCurrentCoin: React.Dispatch<React.SetStateAction<UserCoin>>;
+  isBuyMenuOpen: boolean;
+  setIsBuyMenuOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  buy: () => void;
+}
+
+export const BuyContext = createContext<BuyContext>({} as BuyContext);
 
 const App = () => {
   const [listOfCoins, setListOfCoins] = useState<APICoin[]>([]);
@@ -32,6 +42,33 @@ const App = () => {
     quantity: 0,
     buyPrice: 0,
   });
+  const [isBuyMenuOpen, setIsBuyMenuOpen] = useState(false);
+
+  const buy = () => {
+    setIsBuyMenuOpen(false);
+    setUserPortfolio((prevUserPortfolio: UserCoin[]) => {
+      if (
+        prevUserPortfolio.some((coin: UserCoin) => coin.id === currentCoin.id)
+      ) {
+        return prevUserPortfolio.map((coin: UserCoin) => {
+          if (coin.id === currentCoin.id) {
+            return {
+              ...coin,
+              quantity: +coin.quantity + +currentCoin.quantity,
+            };
+          }
+          return coin;
+        });
+      }
+
+      return prevUserPortfolio.concat({
+        id: currentCoin.id,
+        quantity: +currentCoin.quantity,
+        buyPrice: +listOfCoins.filter((coin) => coin.id === currentCoin.id)[0]
+          ?.priceUsd,
+      });
+    });
+  };
 
   useEffect(() => {
     document.addEventListener("scroll", scrollHandler);
@@ -50,10 +87,15 @@ const App = () => {
     }
   }, [fetching]);
 
-  const scrollHandler = (e: React.UIEvent<DocumentAndElementEventHandlers>) => {
+  const scrollHandler = () => {
+    console.log(
+      window.innerHeight,
+      document.documentElement.scrollTop,
+      document.documentElement.scrollHeight
+    );
     if (
-      window.innerHeight + (e.target as Document).documentElement.scrollTop >
-      (e.target as Document).documentElement.scrollHeight - 100
+      window.innerHeight + document.documentElement.scrollTop >
+      document.documentElement.scrollHeight - 100
     ) {
       setFetching(true);
       setLimit((prevLimit) => prevLimit + 10);
@@ -68,23 +110,33 @@ const App = () => {
           userPortfolio={userPortfolio}
           setUserPortfolio={setUserPortfolio}
         />
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <Main
-                currentCoin={currentCoin}
-                setCurrentCoin={setCurrentCoin}
-                listOfCoins={listOfCoins}
-                setUserPortfolio={setUserPortfolio}
-              />
-            }
-          />
-          <Route
-            path=":coinId"
-            element={<CoinPage listOfCoins={listOfCoins} />}
-          />
-        </Routes>
+        <BuyContext.Provider
+          value={{
+            currentCoin,
+            setCurrentCoin,
+            isBuyMenuOpen,
+            setIsBuyMenuOpen,
+            buy,
+          }}
+        >
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <Main
+                  currentCoin={currentCoin}
+                  setCurrentCoin={setCurrentCoin}
+                  listOfCoins={listOfCoins}
+                  setUserPortfolio={setUserPortfolio}
+                />
+              }
+            />
+            <Route
+              path=":coinId"
+              element={<CoinPage listOfCoins={listOfCoins} buy={buy} />}
+            />
+          </Routes>
+        </BuyContext.Provider>
       </BrowserRouter>
     </div>
   );
