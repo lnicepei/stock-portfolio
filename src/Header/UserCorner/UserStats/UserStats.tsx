0d0dd27b1b6c31bf60ec8f62e717da9623a48c8f
lnicepei <dmitrii.shaidorov@gmail.com>
2022-11-13 11:57:38 +0300

    Change props types according to AdditionalCoinInfo

--- src/Header/UserCorner/UserStats/UserStats.tsx
+++ src/Header/UserCorner/UserStats/UserStats.tsx
@@ -6,21 +6,21 @@ const StyledUserStats = styled.ul`
 `;
 
 type Props = {
-  currentMoney: string;
-  difference: number;
-  percents: number;
+  currentMoney: number;
+  difference: string;
+  percents: string;
 };
 
 const UserStats: React.FC<Props> = ({ currentMoney, difference, percents }) => {
   const StyledP = styled.li`
-    color: ${difference < 0 ? "red" : "green"};
+    color: ${+difference < 0 ? "red" : "green"};
   `;
 
   return (
     <StyledUserStats>
       <li>{Number(currentMoney).toFixed(2)}$</li>
       <StyledP>
-        {difference > 0 && "+"}
+        {+difference > 0 && "+"}
         {difference}$
       </StyledP>
       <StyledP>{percents}%</StyledP>
