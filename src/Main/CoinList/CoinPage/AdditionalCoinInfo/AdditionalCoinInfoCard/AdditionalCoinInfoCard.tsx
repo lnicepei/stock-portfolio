08785ca408023d476c687e2dd84dba5c6ec751d2
lnicepei <dmitrii.shaidorov@gmail.com>
2022-11-13 11:44:28 +0300

    Handle returning links to explorer website

--- src/Main/CoinList/CoinPage/AdditionalCoinInfo/AdditionalCoinInfoCard/AdditionalCoinInfoCard.tsx
+++ src/Main/CoinList/CoinPage/AdditionalCoinInfo/AdditionalCoinInfoCard/AdditionalCoinInfoCard.tsx
@@ -1,16 +1,41 @@
-import { Card } from "../../../../../App/App.styles";
+import styled from "styled-components";
 
 type Props = {
   parameter: string;
   info: string;
-}
+};
+
+const StyledFieldset = styled.fieldset`
+  display: flex;
+  align-items: center;
+  flex: 1 1;
+  margin: 5px;
+  padding: 5px;
+  border: 1px solid #e7ecef;
+  box-shadow: 0 2px 4px rgba(33, 36, 41, 0.05);
+  box-sizing: border-box;
+  border-radius: 10px;
+  transition: all 0.1s ease-in-out;
+  user-select: none;
+  color: #fff;
+
+  legend {
+    text-transform: capitalize;
+  }
+`;
 
 const AdditionalCoinInfoCard: React.FC<Props> = ({ parameter, info }) => {
   return (
-    <Card key={parameter}>
-      <p>{parameter}</p>
-      <p>{":" + info}</p>
-    </Card>
+    <StyledFieldset key={parameter}>
+      <legend>{parameter}</legend>
+      {parameter === "explorer" ? (
+        <a href={info} target="_blank">
+          {info}
+        </a>
+      ) : (
+        <p>{info}</p>
+      )}
+    </StyledFieldset>
   );
 };
 
