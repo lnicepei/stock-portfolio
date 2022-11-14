1bb85e320bbe4da27c4a24da3470aea8e54b5e8b
lnicepei <dmitrii.shaidorov@gmail.com>
2022-11-14 21:37:59 +0300

    Show cross if modal is open

--- src/components/ModalToggle/ModalToggle.tsx
+++ src/components/ModalToggle/ModalToggle.tsx
@@ -23,10 +23,17 @@ const ModalToggle: React.FC<Props> = ({
     <>
       <StyledModalToggle onClick={toggleModal}>
         <svg width={24} viewBox="0 0 24 24">
-          <path
-            fill="currentColor"
-            d="M10,2H14A2,2 0 0,1 16,4V6H20A2,2 0 0,1 22,8V19A2,2 0 0,1 20,21H4C2.89,21 2,20.1 2,19V8C2,6.89 2.89,6 4,6H8V4C8,2.89 8.89,2 10,2M14,6V4H10V6H14Z"
-          />
+          {isModalOpen ? (
+            <path
+              fill="currentColor"
+              d="M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z"
+            />
+          ) : (
+            <path
+              fill="currentColor"
+              d="M10,2H14A2,2 0 0,1 16,4V6H20A2,2 0 0,1 22,8V19A2,2 0 0,1 20,21H4C2.89,21 2,20.1 2,19V8C2,6.89 2.89,6 4,6H8V4C8,2.89 8.89,2 10,2M14,6V4H10V6H14Z"
+            />
+          )}
         </svg>
       </StyledModalToggle>
       {isModalOpen && (
