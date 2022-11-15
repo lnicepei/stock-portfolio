4650e23587b0976e7ae02adf3d32d6d5aa2cb33d
lnicepei <dmitrii.shaidorov@gmail.com>
2022-11-15 17:15:05 +0300

    Add BlurBackground div, style existing inputs

--- src/components/BuyMenu/style.ts
+++ src/components/BuyMenu/style.ts
@@ -1,33 +1,56 @@
 import styled from "styled-components";
 import { Button } from "../../App/style";
 
+export const BlurBackground = styled.div`
+  display: flex;
+  flex-direction: column;
+  background-color: #5eb2f739;
+  backdrop-filter: blur(10px);
+  padding: 5vw;
+  box-sizing: border-box;
+  border-radius: 20px;
+`;
+
 export const StyledBuyMenu = styled.div`
+  position: fixed;
+  top: 50%;
+  left: 50%;
   display: flex;
   justify-content: center;
   align-items: center;
   flex-direction: column;
-  position: fixed;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
   height: 100vh;
   width: 100vw;
-  border-radius: 20px;
-  background-color: #5eb2f739;
+  transform: translate(-50%, -50%);
+  background-color: rgba(0, 0, 0, 0.5);
   box-sizing: border-box;
-  padding: 200px;
-  backdrop-filter: blur(10px);
-  background-clip: content-box, padding-box;
   z-index: 3;
 `;
 
-export const StyledBuyText = styled.h2`
+export const StyledBuyText = styled.h1`
+  color: white;
+  margin-top: 0;
+`;
+
+export const StyledCloseButton = styled.div`
+  position: absolute;
+  right: 0;
+  top: 0;
+  margin: 0;
+  box-sizing: content-box;
+  padding: 10px;
+  cursor: pointer;
   color: white;
 `;
 
-export const StyledCloseButton = styled(Button)`
-  position: absolute;
-  right: 200px;
-  top: 220px;
-  cursor: pointer;
+export const StyledInput = styled.input`
+  font-size: 18px;
+  padding: 10px;
+  margin: 10px 0;
+  border: none;
+  border-radius: 3px;
+`;
+
+export const StyledBuyButton = styled(Button)`
+  margin: 0;
 `;
