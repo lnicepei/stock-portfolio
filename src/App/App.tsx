2f4024a344219186be5a8c4c3c9f8f658fa0ea96
lnicepei <dmitrii.shaidorov@gmail.com>
2022-11-16 09:25:40 +0300

    Set portfolio on App mounting

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,6 +1,5 @@
-import React, { createContext, useEffect, useState } from "react";
+import React, { createContext, useEffect, useRef, useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { fetchListOfCoins } from "../api";
 import Header from "../components/Header/Header";
 import CoinPage from "../pages/CoinPage/CoinPage";
 import Main from "../pages/MainPage/MainPage";
@@ -17,85 +16,35 @@ interface BuyContext {
 export const BuyContext = createContext<BuyContext>({} as BuyContext);
 
 const App = () => {
-  const [listOfCoins, setListOfCoins] = useState<APICoin[]>([]);
-  const [limit, setLimit] = useState(30);
-  const [fetching, setFetching] = useState(true);
-  const [userPortfolio, setUserPortfolio] = useState<UserCoin[]>(() => [
-    {
-      id: "bitcoin",
-      quantity: 0.14231,
-      buyPrice: 16705.19397,
-    },
-    {
-      id: "tether",
-      quantity: 0.1234231,
-      buyPrice: 0.9995,
-    },
-    {
-      id: "binance-coin",
-      quantity: 0.1234231,
-      buyPrice: 278.44926,
-    },
-  ]);
+  const mainRef = useRef(null);
+  const [isBuyMenuOpen, setIsBuyMenuOpen] = useState(false);
   const [currentCoin, setCurrentCoin] = useState<UserCoin>({
     id: "",
     quantity: 0,
     buyPrice: 0,
   });
-  const [isBuyMenuOpen, setIsBuyMenuOpen] = useState(false);
+  const [listOfCoins, setListOfCoins] = useState<APICoin[]>([]);
+  const [userPortfolio, setUserPortfolio] = useState<UserCoin[]>(() =>
+    localStorage.getItem("portfolio") === null
+      ? []
+      : JSON.parse(localStorage.getItem("portfolio") ?? "{}")
+  );
 
   const buy = () => {
     setIsBuyMenuOpen(false);
-    setUserPortfolio((prevUserPortfolio: UserCoin[]) => {
-      if (
-        prevUserPortfolio.some((coin: UserCoin) => coin.id === currentCoin.id)
-      ) {
-        return prevUserPortfolio.map((coin: UserCoin) => {
-          if (coin.id === currentCoin.id) {
-            return {
-              ...coin,
-              quantity: +coin.quantity + +currentCoin.quantity,
-            };
-          }
-          return coin;
-        });
-      }
-
-      return prevUserPortfolio.concat({
+    setUserPortfolio((prevUserPortfolio: UserCoin[]) =>
+      prevUserPortfolio.concat({
         id: currentCoin.id,
         quantity: +currentCoin.quantity,
         buyPrice: +listOfCoins.filter((coin) => coin.id === currentCoin.id)[0]
           ?.priceUsd,
-      });
-    });
-  };
-
-  useEffect(() => {
-    document.addEventListener("scroll", scrollHandler);
-
-    return () => document.removeEventListener("scroll", scrollHandler);
-  }, []);
-
-  useEffect(() => {
-    if (fetching) {
-      fetchListOfCoins(limit, setListOfCoins, setFetching);
-    }
-  }, [fetching]);
-
-  const scrollHandler = () => {
-    console.log(
-      window.innerHeight,
-      document.documentElement.scrollTop,
-      document.documentElement.scrollHeight
+      })
     );
-    if (
-      window.innerHeight + document.documentElement.scrollTop >
-      document.documentElement.scrollHeight - 150
-    ) {
-      setFetching(true);
-      setLimit((prevLimit) => prevLimit + 10);
-    }
   };
+
+  useEffect(() => {
+    localStorage.setItem("portfolio", JSON.stringify(userPortfolio));
+  }, [userPortfolio]);
 
   return (
     <StyledApp>
@@ -119,10 +68,9 @@ const App = () => {
               path="/"
               element={
                 <Main
-                  currentCoin={currentCoin}
-                  setCurrentCoin={setCurrentCoin}
+                  innerRef={mainRef}
                   listOfCoins={listOfCoins}
-                  setUserPortfolio={setUserPortfolio}
+                  setListOfCoins={setListOfCoins}
                 />
               }
             />
